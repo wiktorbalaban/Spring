48b0bd6642274d627d7f9d15c9906fc5dd9c115b
Wiktor <wiktorbalaban@gmail.com>
2018-01-11 09:00:52 +0100

    Stworzenia środowiska

---
+++ src/test/java/Przyrost3/entities/SpringBootWebApplicationTest.java
@@ -0,0 +1,16 @@
+package Przyrost3.entities;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootWebApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
